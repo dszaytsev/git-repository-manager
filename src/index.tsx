9cbe230932c22f4e69e79d9e6e4e11a371bc6525
Dmitry <zadima@gmail.com>
2019-10-04 00:31:30 +0300

    remove comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import './variables.css'
+
 import App from './App'
+
 import * as serviceWorker from './serviceWorker'
+
+import './variables.css'
+import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
