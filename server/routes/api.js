b8036c2f99db8cdfd291b131d04c11c0ee2cdbb0
Dmitry <zadima@gmail.com>
2019-10-10 21:02:11 +0300

    fix delete controller

--- server/routes/api.js
+++ server/routes/api.js
@@ -8,7 +8,7 @@ const blobController = require('../api/blob')
 
 api.get('/', reposController.get)
 api.post('/', reposController.post)
-api.delete('/:repositoryId', reposController.delete)
+api.delete('/:repositoryId', reposController.repoDelete)
 api.get('/:repositoryId/commits/:commitHash?', commitsController.getCommits)
 api.get('/:repositoryId/commits/:commitHash/diff', commitsController.diff)
 api.get('/:repositoryId/tree/:path([^/]*)?', contentController.repoContent) // *TODO: make (/tree...)? | Created at: 14.Sep.2019
